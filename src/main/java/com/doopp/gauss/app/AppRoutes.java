13eb9bf79d0a7b30e4504caf6695c3d763a5a8ad
koocyton <koocyton@gmail.com>
2019-01-30 08:58:00 +0800

    添加 public 到静态文件

--- src/main/java/com/doopp/gauss/app/AppRoutes.java
+++ src/main/java/com/doopp/gauss/app/AppRoutes.java
@@ -1,9 +1,12 @@
 package com.doopp.gauss.app;
 
 import com.doopp.gauss.app.handle.HelloHandle;
+import com.doopp.gauss.server.util.JarToolUtil;
 import com.google.inject.Inject;
 import reactor.netty.http.server.HttpServerRoutes;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.function.Consumer;
 
 public class AppRoutes {
@@ -11,7 +14,22 @@ public class AppRoutes {
     @Inject
     private HelloHandle helloHandle;
 
-    public Consumer<HttpServerRoutes> routesConsumer() {
+    public Consumer<HttpServerRoutes> getRoutesConsumer() {
+
+        System.out.print("\n" + getClass().getResource("") + "\n");
+        System.out.print("\n" + getClass().getResource("/resources") + "\n");
+        System.out.print("\n" + getClass().getResource("/resources/public") + "\n");
+        System.out.print("\n" + getClass().getResourceAsStream("/public") + "\n");
+        System.out.print("\n" + getClass().getResource("/public") + "\n");
+
+        try {
+            Path resource = JarToolUtil.getJarName().contains("jar")
+                ? Paths.get(getClass().getResource("/resources/public").toURI())
+                : Paths.get(getClass().getResource("/public").toURI());
+        }
+        catch(Exception e ) {
+
+        }
         return routes -> routes
             .get("/hello", (req, res) -> res.sendString(
                 helloHandle.hello()
--- src/main/java/com/doopp/gauss/server/netty/NettyServer.java
+++ src/main/java/com/doopp/gauss/server/netty/NettyServer.java
@@ -1,52 +1,20 @@
 package com.doopp.gauss.server.netty;
 
-import com.doopp.gauss.app.handle.HelloHandle;
-import com.doopp.gauss.server.util.JarToolUtil;
+import com.doopp.gauss.app.AppRoutes;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
-import reactor.netty.http.server.HttpServerRoutes;
-
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.function.Consumer;
 
 public class NettyServer {
 
     @Inject
-    private Injector injector;
-
-    @Inject
-    private HelloHandle helloHandle;
+    private AppRoutes appRoutes;
 
     public void run() throws URISyntaxException {
 
-        System.out.print("\n" + getClass().getResource("") + "\n");
-        System.out.print("\n" + getClass().getResource("/resources") + "\n");
-        System.out.print("\n" + getClass().getResource("/resources/public") + "\n");
-        System.out.print("\n" + getClass().getResourceAsStream("/public") + "\n");
-        System.out.print("\n" + getClass().getResource("/public") + "\n");
-
-        Path resource = JarToolUtil.getJarName().contains("jar")
-                ? Paths.get(getClass().getResource("/resources/public").toURI())
-                : Paths.get(getClass().getResource("/public").toURI());
-        Consumer<HttpServerRoutes> routesConsumer = routes -> routes
-                .get("/hello", (req, res) -> res.sendString(
-                        helloHandle.hello()
-                ))
-                .get("/boy", (req, res) -> res.sendString(
-                        helloHandle.boy(req)
-                ))
-                .ws("/game", (in, out) -> out.send(
-                        helloHandle.game(in.receive())
-                ))
-                .directory("/", resource);
-
         DisposableServer disposableServer = HttpServer.create()
-                .route(routesConsumer)
+                .route(appRoutes.getRoutesConsumer())
                 .host("127.0.0.1")
                 .port(8090)
                 .bind()
