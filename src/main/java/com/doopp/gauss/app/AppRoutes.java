301ffe52455f4908ed27dc6ff8d360399964aef1
yi.liu <liuyi@doopp.com>
2019-02-03 12:21:19 +0800

    fix some

--- src/main/java/com/doopp/gauss/app/AppRoutes.java
+++ src/main/java/com/doopp/gauss/app/AppRoutes.java
@@ -1,6 +1,7 @@
 package com.doopp.gauss.app;
 
 import com.doopp.gauss.app.handle.HelloHandle;
+import com.doopp.gauss.server.netty.handler.StaticFileResourceHandler;
 import com.google.gson.Gson;
 import com.google.inject.Inject;
 import io.netty.handler.codec.http.HttpHeaderNames;
@@ -9,13 +10,16 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
 import reactor.netty.NettyOutbound;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 import reactor.netty.http.server.HttpServerRoutes;
 
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.function.BiFunction;
@@ -31,34 +35,32 @@ public class AppRoutes {
     @Inject
     private Gson gson;
 
-    private final Path publicPath = getPublicPath();
-
-    AppRoutes() throws URISyntaxException {
-
-    }
-
     public Consumer<HttpServerRoutes> getRoutesConsumer() throws URISyntaxException, NoSuchMethodException {
 
         // Path publicPath = this.getPublicPath();
 
         return routes -> routes
+                .get("/index.html", (req, resp) -> {
+                    resp.withConnection(connection -> {
+                        connection.addHandlerLast(new StaticFileResourceHandler());
+                    });
+                    return resp.status(HttpResponseStatus.ACCEPTED);
+                })
                 .get("/user/{id}", (req, resp) -> {
                     Long id = Long.valueOf(req.param("id"));
                     return sendJson(req, resp, helloHandle.hello(id));
                 })
                 .ws("/game", (in, out) -> out.send(
                     helloHandle.game(in.receive())
-                ))
-                .directory("/", publicPath);
+                ));
     }
 
     private Path getPublicPath() throws URISyntaxException {
-        // logger.info("{}", getClass().getResource(""));
-        // logger.info("{}", getClass().getResource("/resources"));
-        // logger.info("{}", getClass().getResource("/resources/public"));
-        // logger.info("{}", getClass().getResourceAsStream("/public"));
-        // logger.info("{}", getClass().getResource("/public"));
-        return Paths.get(AppRoutes.class.getResource("/public").toURI());
+        //logger.info("{}", getClass().getResourceAsStream("/public"));
+        InputStream publicIs = AppRoutes.class.getResourceAsStream("/public");
+        URL publicUrl = AppRoutes.class.getResource("/public");
+        logger.info("\n>>>{}\n>>>{}\n>>>{}", publicUrl, publicUrl.toString(), publicIs);
+        return Paths.get(publicUrl.toURI());
     }
 
     private <T> NettyOutbound sendJson(HttpServerRequest req, HttpServerResponse resp, T handle) {
