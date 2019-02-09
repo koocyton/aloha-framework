108b5b44270e5a0cb17008a8cac112639bcb7353
yi.liu <liuyi@doopp.com>
2019-02-09 17:34:01 +0800

    每次进步一丢丢

--- src/main/java/com/doopp/gauss/server/netty/AppRoute.java
+++ src/main/java/com/doopp/gauss/server/netty/AppRoute.java
@@ -3,16 +3,20 @@ package com.doopp.gauss.server.netty;
 import com.doopp.gauss.app.handle.HelloHandle;
 import com.google.inject.Injector;
 import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
+import reactor.netty.NettyPipeline;
 import reactor.netty.http.server.HttpServerRoutes;
 import reactor.netty.http.websocket.WebsocketInbound;
 import reactor.netty.http.websocket.WebsocketOutbound;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.function.Consumer;
 import java.util.logging.SocketHandler;
@@ -42,30 +46,60 @@ public class AppRoute {
                 })
                 .ws("/game2", this::wsHandler)
                 .ws("/game4", (in, out) -> out
-                    .sendString(Mono.just("Hello World!"))
-                    .then(in.receive()
-                        .asString()
-                        .next()
-                        .log()
-                        .then())
+                        .sendString(Mono.just("Hello World!"))
+                        .then(in.receive()
+                                .asString()
+                                .next()
+                                .log()
+                                .then())
                 )
-                .ws("/game", (in, out) -> {
-                    return out.send(
-                        in.withConnection(connection -> {
-                            in.aggregateFrames()
-                                    .receiveFrames()
-                                    .map(WebSocketFrame::content)
-                                    .map(byteBuf -> byteBuf
-                                            .readCharSequence(byteBuf.readableBytes(), Charset.defaultCharset()).toString())
-                            .map(i -> new TextWebSocketFrame(i + ""));
-                            connection.addHandlerLast(new WebSocketFrameHandler());
-                        }).receive()
+                .ws("/game5", (in, out) -> {
+                    return out.options(NettyPipeline.SendOptions::flushOnEach).send(
+                            in.withConnection(connection -> {
+                                in.aggregateFrames()
+                                        .receiveFrames()
+                                        .map(WebSocketFrame::content)
+                                        .map(byteBuf -> byteBuf
+                                                .readCharSequence(byteBuf
+                                                        .readableBytes(), Charset
+                                                        .defaultCharset())
+                                                .toString())
+                                        .map(TextWebSocketFrame::new)
+                                        .map(i -> new TextWebSocketFrame(i + ""));
+                                // connection.addHandlerLast(new WebSocketFrameHandler());
+                            }).receive()
                     );
                 })
-                .ws("/game3", (in, out) -> appOutbound.sendWs(
+                .ws("/game6", (in, out) -> appOutbound.sendWs(
                         in, out, injector.getInstance(HelloHandle.class).game(in, out)
                         )
                 )
+                .ws("/game", (in, out) -> {
+                    AA aa = new AA();
+                    return out
+                            .sendObject(in
+                                    .withConnection(connection -> {
+                                        connection
+                                                .channel()
+                                                .pipeline()
+                                                // .deregister()
+                                                .writeAndFlush(new TextWebSocketFrame("hello 1 "))
+                                                .addListener((ChannelFutureListener) future -> {
+                                                    if (future.isSuccess()) {
+                                                        future.channel().writeAndFlush(new TextWebSocketFrame("hello 2    ss3"));
+                                                        Thread.sleep(1000);
+                                                        logger.info("Thread.sleep(1000)");
+                                                    }
+                                                });
+                                    })
+                                    .receive()
+                                    .subscribe(
+                                            tick -> logger.info("Tick {}", tick),
+                                            (ex) -> logger.info("Error emitted"),
+                                            () -> logger.info("Completed")
+                                    )
+                            );
+                })
                 .get("/**", appOutbound::sendStatic);
     }
 
@@ -76,5 +110,20 @@ public class AppRoute {
                     connection.addHandlerLast(new WebSocketFrameHandler());
                 }).receive()
         );
+    }
+
+    private class AA {
+
+        public void bb() {
+
+        }
+
+        public void cc() {
+
+        }
+
+        public void dd() {
+
+        }
     }
 }
