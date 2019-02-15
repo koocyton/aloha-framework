7da62ed7a64804123e568090477c104665871985
yi.liu <liuyi@doopp.com>
2019-02-15 17:08:51 +0800

    提交一下

--- src/main/java/com/doopp/gauss/server/message/CommonResponse.java
+++ src/main/java/com/doopp/gauss/server/message/CommonResponse.java
@@ -1,8 +1,5 @@
 package com.doopp.gauss.server.message;
 
-import lombok.Data;
-
-@Data
 public class CommonResponse<T> {
 
     private int err_code = 0;
@@ -10,6 +7,18 @@ public class CommonResponse<T> {
     private String err_msg = "";
 
     private T data;
+
+    public void setErr_code(int err_code) {
+        this.err_code = err_code;
+    }
+
+    public void setErr_msg(String err_msg) {
+        this.err_msg = err_msg;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
 
     public CommonResponse(T data) {
         this.data = data;
--- src/main/java/com/doopp/gauss/server/message/response/SessionToken.java
+++ src/main/java/com/doopp/gauss/server/message/response/SessionToken.java
@@ -1,13 +1,18 @@
 package com.doopp.gauss.server.message.response;
 
-import lombok.Data;
-
-@Data
 public class SessionToken {
 
     private String token = "";
 
     private int expire = 7776000;
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setExpire(int expire) {
+        this.expire = expire;
+    }
 
     public SessionToken(String token) {
         this.setToken(token);
