84b979d574b8123c6f0cd189ebaa244f87f4721a
yi.liu <liuyi@doopp.com>
2019-03-20 13:36:51 +0800

    完美

--- src/main/java/com/doopp/gauss/oauth/handle/GameWsHandle.java
+++ src/main/java/com/doopp/gauss/oauth/handle/GameWsHandle.java
@@ -1,12 +1,14 @@
 package com.doopp.gauss.oauth.handle;
 
 import com.doopp.gauss.server.handle.AbstractWebSocketServerHandle;
+import com.google.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.ws.rs.Path;
 
 @Slf4j
 @Path("/manage/chat/ws")
+@Singleton
 public class GameWsHandle extends AbstractWebSocketServerHandle {
 
 }
